eff43116cc89d3ab261b767a6c3b9df4aad6e414
hikimayuuki <yuuki.hikima@gmail.com>
2020-05-16 16:13:17 +0900

    課題提出

--- practixe.php
+++ practixe.php
@@ -1,34 +1,36 @@
+<?php
+
 $a = 3;
 $b = 7;
-echo = $a + $b
+echo  $a + $b . "\n";
 
-$array_month = ［1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12］;
+$array_month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 echo $array [7];
 
 $hello = "Hello,";
 $name = "hikima";
-$world = "'s World!"
-echo $hello.$name.$world;
+$world = "'s World!";
+echo $hello.$name.$world . "\n";
 
 $value = 'tech';
 $value .= ' boost';
-echo $value;
+echo $value . "\n";
 
 
-$2018_calendar = {
+$calendar2018 = [
   "January" => "1月",
   "February" => "2月",
   "March" => "3月",
   "April" => "4月",
   "May" => "5月",
-  "June" = "6月",
+  "June" => "6月",
   "July" => "7月",
   "August" => "8月",
   "September" => "9月",
-  "October" => 10月,
+  "October" => "10月",
   "November" => "11月",
   "December" => "12月"
-};
+];
 
 // 12月を表示する
-echo $2018_calendar;
+echo $calendar2018["December"] . "\n";
