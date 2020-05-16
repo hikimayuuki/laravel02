6a8ff013743c16254df860860109b9f848d67069
hikimayuuki <yuuki.hikima@gmail.com>
2020-05-16 15:46:32 +0900

    課題提出

---
+++ practixe.php
@@ -0,0 +1,34 @@
+$a = 3;
+$b = 7;
+echo = $a + $b
+
+$array_month = ［1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12］;
+echo $array [7];
+
+$hello = "Hello,";
+$name = "hikima";
+$world = "'s World!"
+echo $hello.$name.$world;
+
+$value = 'tech';
+$value .= ' boost';
+echo $value;
+
+
+$2018_calendar = {
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" = "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => 10月,
+  "November" => "11月",
+  "December" => "12月"
+};
+
+// 12月を表示する
+echo $2018_calendar;
